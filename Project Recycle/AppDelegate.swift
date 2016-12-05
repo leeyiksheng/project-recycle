61775125d212361ca1b71c31c5e511192b98ad84
KevinSia <calvin.kl1026@gmail.com>
2016-12-05 10:26:06 +0800

    fix some target error

--- Project Recycle/AppDelegate.swift
+++ Project Recycle/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if FIRAuth.auth()?.currentUser != nil {
 
     
-            window!.rootViewController = instantiateKelvinViewController()
+//            window!.rootViewController = instantiateKelvinViewController()
 
 
             // window!.rootViewController = instantiateDriverViewController()
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
             //window!.rootViewController = instantiateHomeContainerViewController()
             // window!.rootViewController = instantiateLoginViewController()
-            //window!.rootViewController = instantiateUserViewController()
+            window!.rootViewController = instantiateUserViewController()
             
             FIRAuth.auth()?.addStateDidChangeListener { auth, user in
                 if user != nil {
