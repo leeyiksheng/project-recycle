0dc32976168aa827edab9e29a7c039ce34b089ac
Kelvin Lee <leeweisern@gmail.com>
2016-12-15 16:24:35 +0800

    icons added

--- Project Recycle/MenuTabBarController.swift
+++ Project Recycle/MenuTabBarController.swift
@@ -14,23 +14,23 @@ class MenuTabBarController: UITabBarController {
         super.viewDidLoad()
 
         let homeItem = RecycleGeneralViewController()
-        let homeItemIcon = UITabBarItem(title: "Home", image: UIImage(named: "redErrorIcon"), selectedImage: UIImage(named: "redErrorIcon"))
+        let homeItemIcon = UITabBarItem(title: "Home", image: UIImage(named: "home"), selectedImage: UIImage(named: "home"))
         homeItem.tabBarItem = homeItemIcon
         
         let profileStoryboard = UIStoryboard.init(name: "Profile", bundle: Bundle.init(identifier: "Profile"))
         let profileItem = profileStoryboard.instantiateViewController(withIdentifier: "Profile")
-        let profileItemIcon = UITabBarItem(title: "Profile", image: UIImage(named: "redErrorIcon"), selectedImage: UIImage(named: "redErrorIcon"))
+        let profileItemIcon = UITabBarItem(title: "Profile", image: UIImage(named: "profile"), selectedImage: UIImage(named: "profile"))
         profileItem.tabBarItem = profileItemIcon
         
         let ordersStoryboard = UIStoryboard.init(name: "Orders", bundle: Bundle.init(identifier: "Orders"))
         let ordersItem = ordersStoryboard.instantiateViewController(withIdentifier: "Orders")
-        let ordersItemIcon = UITabBarItem(title: "Orders", image: UIImage(named: "redErrorIcon"), selectedImage: UIImage(named: "redErrorIcon"))
+        let ordersItemIcon = UITabBarItem(title: "Orders", image: UIImage(named: "orders"), selectedImage: UIImage(named: "orders"))
         ordersItem.tabBarItem = ordersItemIcon
         
        
         let guideViewController = GuideViewController()
         let guideItem = UINavigationController(rootViewController: guideViewController)
-        let guideItemIcon = UITabBarItem(title: "Guide", image: UIImage(named: "redErrorIcon"), selectedImage: UIImage(named: "redErrorIcon"))
+        let guideItemIcon = UITabBarItem(title: "Guide", image: UIImage(named: "guide"), selectedImage: UIImage(named: "guide"))
         guideItem.tabBarItem = guideItemIcon
         
         let viewControllers = [homeItem, ordersItem, guideItem, profileItem]
